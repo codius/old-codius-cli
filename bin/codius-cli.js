029b684a5096a9b86f36dd7743ef3ae9ca13aa1a
Vernon Johnson <vernjohn@Vernons-MBP-2.attlocal.net>
2018-05-29 08:27:56 -0700

    feat: add yarg commands and options

--- bin/codius-cli.js
+++ bin/codius-cli.js
@@ -1,6 +1,48 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
-const Codius-cli = require('../src')
+const CodiusCli = require('../src')
 const Config = require('../src/config')
 
-
+const yargs = require('yargs')
+  .option('manifest', {
+    alias: 'm',
+    // TODO: set default manifest path
+    type: 'string',
+    description: 'Manifest file'
+  })
+  .option('nonce', {
+    alias: 'n',
+    type: 'boolean',
+    default: false,
+    description: 'Generate nonce for manifest hash'
+  })
+  .option('duration', {
+    alias: 'd',
+    type: 'number',
+    default: 3600,
+    description: 'Duration the contract should run (seconds)'
+  })
+  .option('max-price', {
+    alias: 'mp',
+    type: 'number',
+    // TODO: set default max-price
+    description: 'Max price for contract'
+  })
+  .option('hash-manifest', {
+    alias: 'hm',
+    type: 'boolean',
+    default: false,
+    description: 'Generate manifest hash'
+  })
+  .option('contract', {
+    alias: 'c',
+    type: 'string',
+    description: 'Contract id to extend'
+  })
+  .command('start', 'Start contract upload process', {}, argv => {
+    console.log('uploading contract...')
+  })
+  .command('*', '', {}, argv => {
+    console.error('unknown command.')
+    process.exit(1)
+  }).argv
