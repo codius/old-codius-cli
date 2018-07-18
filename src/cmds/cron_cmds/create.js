f4b1339f3a55ce6dfd196521fcee1e035343b777
Vernon Johnson <vtj2105@columbia.edu>
2018-07-18 14:57:53 -0700

    chore: use riverpig for logging errors with create cmd

--- src/cmds/cron_cmds/create.js
+++ src/cmds/cron_cmds/create.js
@@ -7,7 +7,7 @@ exports.desc = 'Create a new cron job to extend a pod.'
 exports.builder = cronExtendOptions
 exports.handler = async function (argv) {
   if (!argv.bufferSec && !argv.skipExtend) {
-    console.error('Must specify a buffer or use the `--skip-extend` flag to skip the initial pod extend step.')
+    logger.error('Must specify a buffer or use the `--skip-extend` flag to skip the initial pod extend step.')
     process.exit(1)
   }
   logger.debug(`Create cron args: ${JSON.stringify(argv)}`)
