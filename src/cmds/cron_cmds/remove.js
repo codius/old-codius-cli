abb92b5e5e4f6d2a330b6d36daf9fbdbece783a0
Vernon Johnson <vtj2105@columbia.edu>
2018-07-18 15:56:49 -0700

    fix: update arg checks for remove and view commands

--- src/cmds/cron_cmds/remove.js
+++ src/cmds/cron_cmds/remove.js
@@ -6,6 +6,10 @@ exports.command = 'remove [options]'
 exports.desc = 'Remove an existing cron job.'
 exports.builder = cronRemoveOptions
 exports.handler = async function (argv) {
+  if (argv.all && argv.codiusStateFile) {
+    logger.error('Arguments codius-state-file and all are mutually exclusive')
+    process.exit(1)
+  }
   logger.debug(`Remove cron args: ${JSON.stringify(argv)}`)
   removeCron(argv)
 }
--- src/cmds/cron_cmds/view.js
+++ src/cmds/cron_cmds/view.js
@@ -6,6 +6,10 @@ exports.command = 'view [options]'
 exports.desc = 'View existing cron jobs.'
 exports.builder = cronViewOptions
 exports.handler = async function (argv) {
+  if (argv.all && argv.codiusStateFile) {
+    logger.error('Arguments codius-state-file and all are mutually exclusive')
+    process.exit(1)
+  }
   logger.debug(`View cron args: ${JSON.stringify(argv)}`)
   viewCron(argv)
 }
