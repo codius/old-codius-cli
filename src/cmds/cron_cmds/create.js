877985bd6ec4006a1e9d278c5c2f4db4dbc2a454
Vernon Johnson <vtj2105@columbia.edu>
2018-07-19 12:11:34 -0700

    feat: check os before running cron cmds

--- src/cmds/cron_cmds/create.js
+++ src/cmds/cron_cmds/create.js
@@ -1,11 +1,16 @@
 const logger = require('riverpig')('codius-cli:create')
 const { cronExtendOptions } = require('../../cmds/options/options.js')
 const { createCron } = require('../../handlers/cron_handlers/create.js')
+const os = require('os')
 
 exports.command = 'create [options]'
 exports.desc = 'Create a new cron job to extend a pod.'
 exports.builder = cronExtendOptions
 exports.handler = async function (argv) {
+  if (os.type() === 'Windows_NT') {
+    logger.error('Command is not supported on Windows machines')
+    process.exit(1)
+  }
   if (!argv.bufferSec && !argv.skipExtend) {
     logger.error('Must specify buffer-sec or use the skip-extend flag to skip the initial pod extend step.')
     process.exit(1)
--- src/cmds/cron_cmds/view.js
+++ src/cmds/cron_cmds/view.js
@@ -1,11 +1,16 @@
 const logger = require('riverpig')('codius-cli:view')
 const { viewCron } = require('../../handlers/cron_handlers/view.js')
 const { cronViewOptions } = require('../options/options.js')
+const os = require('os')
 
 exports.command = 'view [options]'
 exports.desc = 'View existing cron jobs.'
 exports.builder = cronViewOptions
 exports.handler = async function (argv) {
+  if (os.type() === 'Windows_NT') {
+    logger.error('Command is not supported on Windows machines')
+    process.exit(1)
+  }
   if (argv.all && argv.codiusStateFile) {
     logger.error('Arguments codius-state-file and all are mutually exclusive')
     process.exit(1)
